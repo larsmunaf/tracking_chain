ae0b27febe6669f30c4d94a6fcbbb406b8d92a2e
larsmunaf <lmu001@munaf.de>
2018-10-04 18:28:05 +0200

    added 2nd dapp that reacts to events

--- contracts/Tracking.sol
+++ contracts/Tracking.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.23;
 //pragma experimental ABIEncoderV2;
 
 contract PositionTracking {
@@ -17,5 +17,12 @@ contract PositionTracking {
     function setRandomPosition (uint pos_x, uint pos_y) public returns (uint) {
         currentPos = position ({x:pos_x, y:pos_y});
         emit PositionValue(msg.sender, pos_x, pos_y);
+    }
+
+    string message = "position has changed";
+
+    function notify () public returns (string) {
+        emit PositionValue(msg.sender, currentPos.x, currentPos.y);
+        return message;
     }
 }
